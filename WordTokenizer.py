d1b44a5c1f66bb72e101de6bca34ba00c0b073d9
NameerWaqas <nameerwaqas321@gmail.com>
2020-11-28 15:50:03 +0500

    Feat: Bagodf words i progress.

--- WordTokenizer.py
+++ WordTokenizer.py
@@ -1,32 +1,36 @@
 from ClassPartAllocator import ClassPartAllocator
 import re
 
-    # lineNumber  = 1
-    # temp = "" #To hold value temporarily
-    # tokenSet=[]
-    # for char in charArr:
-    #     if char == " ":
-    #         classPart = ClassPartAllocator(temp)
-    #         if classPart == -1:
-    #             classPart = "Identifier" if re.match("^[a-z|_]([a-zA-Z0-9]{1,25})?$",temp) else "Invalid"
-    #         tokenSet.append((classPart,temp,lineNumber))
-    #         temp=""
-    #     elif char=="=" or char == ";":
-    #         temp = temp + char
-    #         classPart = ClassPartAllocator(char)
-    #         tokenSet.append((classPart,temp,lineNumber))
-    #     else:
-    #         if re.match("^[a-zA-Z0-9]$",char) :
-    #             temp = temp + char
-    #             continue
-    # return tokenSet
+
 def FilterStartingSpaces(charList=[]):
     # return param
     for char in charList:
-        if re.match("^[ ]$",char):
+        if re.match("^[ ]$", char):
             charList.pop(charList.index(char))
         else:
             return charList
 
+
 def WordTokernizer(charList):
-   return FilterStartingSpaces(charList)
+    filteredList = FilterStartingSpaces(charList)
+    lineNumber = 1
+    BagOfWords = []
+    wordBreaker = ['+', '-', '*', '%', '<', '>', '!']
+    alone = ['?', ';', ':', '(', ')', '{', '}', ',', '[', ']']
+
+    temp = ""
+
+    for char in filteredList:
+        if re.match("^[a-zA-Z0-9]$", char):
+            if re.match("^[a-zA-Z0-9|_]$", temp[len(temp)-1]):
+                temp = temp + char
+            else:
+                BagOfWords.append(temp)
+                temp = char
+        elif char in alone:
+            BagOfWords.append(temp)
+            temp = ""
+            BagOfWords.append(char)
+        elif char is " " or char is '\n':
+            BagOfWords.append(temp)
+            temp = ""
