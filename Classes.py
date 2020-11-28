11e0229900e3f4ec71975bb5c81ebbaafd8cd445
NameerWaqas <nameerwaqas321@gmail.com>
2020-11-28 11:52:30 +0500

    Update: Divide Classes into two parts and updated the flow logic.

--- Classes.py
+++ Classes.py
@@ -1,30 +1,34 @@
 def Classes():
-    Classes = {
-        "BTOC" : ['void','jabtak','agar','warna','return','main','class','naya','passive','!','ya','aur','='],
-        "BTOCClassTypes":['Void','Jabtak','Agar','Warna','Return','Main','Class','Naya','Passive'],
+    ClassesAlpha = {
         "Void": ['void'],
         "While": ['jabtak'],
-        "If":['agar'],
-        'Else':['warna'],
-        'Return':['return'],
-        'Main':['main'],
-        'Class':['class'],
-        'New':['naya'],
-        'Abstract':['passive'],
-        'Not':['!'],
-        'Or':['ya'],
-        'And':['aur'],
-        'Equal':['='],
-        "BrkCont": ['roko','jari'], #Break Continue
-        "PM":['+','-'], #Plus Minus class
-        "MDM":['*','/','%'],#Binary operator class
-        "CompOpr":["+=","-=","*=","/=","%="], #Comparision operator class
-        "AssignmentOpr":['='],#Assignment operator class,
-        "TerminatorOpr":[';'],#Comparision operator
-        "Punc":['']
-        "DataType":["int","float","bool","string","char"],# Data types,
-        "Keyword":["main","class","return","function"],# Keywords
-        "AccMod":["ijtemai","infiradi","protected","andruni"], #Access Modifiers
-        "VirOver":['arzi','badlo'] #Virtual Override
+        "If": ['agar'],
+        'Else': ['warna'],
+        'Return': ['return'],
+        'Main': ['main'],
+        'Class': ['class'],
+        'New': ['naya'],
+        'Abstract': ['passive'],
+        'Or': ['ya'],
+        'And': ['aur'],
+        "BrkCont": ['roko', 'jari'],  # Break Continue
+        "DataType": ["int", "float", "bool", "string", "char"],  # Data types,
+        "Keyword": ["main", "class", "return", "function"],  # Keywords
+        # Access Modifiers
+        "AccMod": ["ijtemai", "infiradi", "protected", "andruni"],
+        "VirOver": ['arzi', 'badlo']  # Virtual Override
     }
-    return Classes
+
+    classesSymba = {
+        'Not': ['!'],
+        "PM": ['+', '-'],  # Plus Minus class
+        "MDM": ['*', '/', '%'],  # Binary operator class
+        # Comparision operator class
+        "CompOpr": ["+=", "-=", "*=", "/=", "%="],
+        "AssignmentOpr": ['='],  # Assignment operator class,
+        "TerminatorOpr": [';'],  # Comparision operator
+        "Punc": [''],
+        'Equal': ['=']
+    }
+
+    return [ClassesAlpha,classesSymba]
