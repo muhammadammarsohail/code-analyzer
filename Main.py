a176d6e4cf6d8f8540090fae870b2dc1b40087c5
NameerWaqas <nameerwaqas321@gmail.com>
2020-11-28 12:41:24 +0500

    Feat: Omitting starting spaces.

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__":
     import re #Importing ReGex module
     from WordTokenizer import WordTokernizer
-    str = """int age nameer _ammar main class  = ; 
+    str = """   a int age nameer _ammar main class  = ; 
     """ 
     #may it will come directly from the Text box.
     charArr = list(str) #Character list/array.
--- WordTokenizer.py
+++ WordTokenizer.py
@@ -1,23 +1,32 @@
 from ClassPartAllocator import ClassPartAllocator
 import re
 
-def WordTokernizer(charArr):
-    lineNumber  = 1
-    temp = "" #To hold value temporarily
-    tokenSet=[]
-    for char in charArr:
-        if char == " ":
-            classPart = ClassPartAllocator(temp)
-            if classPart == -1:
-                classPart = "Identifier" if re.match("^[a-z|_]([a-zA-Z0-9]{1,25})?$",temp) else "Invalid"
-            tokenSet.append((classPart,temp,lineNumber))
-            temp=""
-        elif char=="=" or char == ";":
-            temp = temp + char
-            classPart = ClassPartAllocator(char)
-            tokenSet.append((classPart,temp,lineNumber))
+    # lineNumber  = 1
+    # temp = "" #To hold value temporarily
+    # tokenSet=[]
+    # for char in charArr:
+    #     if char == " ":
+    #         classPart = ClassPartAllocator(temp)
+    #         if classPart == -1:
+    #             classPart = "Identifier" if re.match("^[a-z|_]([a-zA-Z0-9]{1,25})?$",temp) else "Invalid"
+    #         tokenSet.append((classPart,temp,lineNumber))
+    #         temp=""
+    #     elif char=="=" or char == ";":
+    #         temp = temp + char
+    #         classPart = ClassPartAllocator(char)
+    #         tokenSet.append((classPart,temp,lineNumber))
+    #     else:
+    #         if re.match("^[a-zA-Z0-9]$",char) :
+    #             temp = temp + char
+    #             continue
+    # return tokenSet
+def FilterStartingSpaces(charList=[]):
+    # return param
+    for char in charList:
+        if re.match("^[ ]$",char):
+            charList.pop(charList.index(char))
         else:
-            if re.match("^[a-zA-Z0-9]$",char) :
-                temp = temp + char
-                continue
-    return tokenSet
+            return charList
+
+def WordTokernizer(charList):
+   return FilterStartingSpaces(charList)
